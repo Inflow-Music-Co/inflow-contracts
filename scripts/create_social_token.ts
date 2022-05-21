9f6cebf6d0b1d6ebfb04bc64b515067d46b20911
Reza <r29taheri@gmail.com>
2022-05-22 02:13:27 +0400

    created deploy mock usdt script

--- scripts/create_social_token.ts
+++ scripts/create_social_token.ts
@@ -1,35 +1,34 @@
 import { ethers } from "hardhat";
 
-const factoryAddress = "0x5c1e4da1d1DC1123b6E6331259dBb41c7cb69F47";
+const factoryAddress = "0x41C659319885598d77CF5bd8E792A5162bC72A04";
 const mintParams = {
-    creator: "0x2E8992A98d58C3CA3b54e8042fF14be36Cc98dB1",
-    usdcCollateral:"0x63aF7615e795F2cFb8A2f93aFAd7CD1B4d35bA5c",
-    usdtCollateral: "0x63aF7615e795F2cFb8A2f93aFAd7CD1B4d35bA5c",
-    maxSupply:"10000000000000000000000000",
-    slope:"500000000000000000",
-    name: "luke",
-    symbol: "LUKE"
-}
+  creator: "0xd5Cdda038127d15Ea44Dd012DFfb9e95a72e6D32",
+  usdcCollateral: "0x63aF7615e795F2cFb8A2f93aFAd7CD1B4d35bA5c",
+  usdtCollateral: "0xb34Ca2cDE88dE520E4Be8b1ccEc374D3052ae021",
+  slope: ethers.utils.parseEther("0.5").toString(),
+  maxSupply: ethers.utils.parseEther("10000000").toString(),
+  name: "luke_test_3",
+  symbol: "LUKETEST3",
+};
 
-
-async function createSocialToken(): Promise<void>{
-    const [owner] = await ethers.getSigners();
-    const factory = await ethers.getContractAt(
-        //ABI
-        "SocialTokenFactory",
-        factoryAddress,
-        owner
-    );
-    const socialTokenCreator = factory.connect(owner);
-    const create = await socialTokenCreator.create(mintParams);
-    console.log("create", create);
+async function createSocialToken(): Promise<void> {
+  const [owner] = await ethers.getSigners();
+  const factory = await ethers.getContractAt(
+    //ABI
+    "SocialTokenFactory",
+    factoryAddress,
+    owner
+  );
+  const socialTokenCreator = factory.connect(owner);
+  const create = await socialTokenCreator.create(mintParams);
+  console.log("create", create);
 }
 
 createSocialToken()
-    .then(async() => {
-        console.log("Successfully sent the transaction");
-    })
-    .catch((err) => {
-        console.error(err);
-        process.exit(1);
-});
+  .then(async () => {
+    console.log("Successfully sent the transaction");
+  })
+  .catch((err) => {
+    console.error(err);
+    process.exit(1);
+  });
