0a16683c10f9c59c4fcf1a97d03b339e93d329d2
Reza <r29taheri@gmail.com>
2022-05-23 02:04:31 +0400

    Added mint and burn total supply and worked on mint in batch test

--- scripts/whitelist.ts
+++ scripts/whitelist.ts
@@ -1,7 +1,7 @@
 import { ethers } from "hardhat";
 
 // fill addresses
-const factoryAddress = "0xa5287A701497156E35d52611575359acdee52da5";
+const factoryAddress = "0x41C659319885598d77CF5bd8E792A5162bC72A04";
 const whiteAddress = "0x2E8992A98d58C3CA3b54e8042fF14be36Cc98dB1";
 
 async function whitelist(whiteAddress: string): Promise<void> {
