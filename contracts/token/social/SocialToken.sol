dba568462798e04bcdcecc7bcd8e88ad745bef40
Richard Melkonian <rvm@Richards-MacBook-Pro.local>
2022-05-27 11:55:07 +0100

    created getMintPriceByCollateral func

--- contracts/token/social/SocialToken.sol
+++ contracts/token/social/SocialToken.sol
@@ -133,7 +133,7 @@ contract SocialToken is ISocialToken, Ownable, ERC20, ReentrancyGuard {
     /// @dev Calculate collateral price to mint
     /// @param amount (uint256): Amount of social tokens to mint
     /// @return (uint256): Collateral required to mint social token amount
-    function getMintPrice(uint256 amount)
+    function getMintPriceBySocialToken(uint256 amount)
         public
         view
         override
@@ -145,8 +145,22 @@ contract SocialToken is ISocialToken, Ownable, ERC20, ReentrancyGuard {
         return
             supply == 0
                 ? (slope * amount * amount) / 2 / 1e48
-                : (((_reserve * newSupply * newSupply) / (supply * supply)) -
-                    _reserve);
+                : (((_reserve * newSupply * newSupply) / (supply * supply)) - _reserve);
+    }
+
+    function getMintPriceByCollateral(uint256 amount) 
+        public
+        view
+        override
+        return (uint256)
+    {
+        uint256 supply = totalSupply();
+        uint256 _reserve = reserve;
+        return
+            supply == 0
+                ? (slope * amount * amount) / 2 / 1e48
+                : (((_reserve * supply * supply) / (supply * supply)) - _reserve);
+
     }
 
     /// @dev Calculate collateral received on burn
